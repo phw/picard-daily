f38930fd3837f0b056cd885e0f6b0f461f9b7189
Philipp Wolfer <ph.wolfer@gmail.com>
2023-08-18 14:27:00 +0200

    Patch version on both Linux and Windows

--- build-appimage.sh
+++ build-appimage.sh
@@ -7,15 +7,17 @@ SOURCE_DIR="$ROOT_DIR/source/"
 
 cd "$ROOT_DIR"
 
-curl -Lo appimagecraft.AppImage "https://github.com/TheAssassin/appimagecraft/releases/download/continuous/appimagecraft-x86_64.AppImage"
-chmod +x appimagecraft.AppImage
-
 [ ! -d "$SOURCE_DIR" ] || rm -rf "$SOURCE_DIR"
 git clone --depth 500 --branch master "https://github.com/metabrainz/picard.git" "$SOURCE_DIR"
 pushd "$SOURCE_DIR"
 git fetch --depth=1 origin "+refs/tags/release-*:refs/tags/release-*"
 patch -p1 < appimage/appimage.patch
 popd
+
+./patch-version.sh
+
+curl -Lo appimagecraft.AppImage "https://github.com/TheAssassin/appimagecraft/releases/download/continuous/appimagecraft-x86_64.AppImage"
+chmod +x appimagecraft.AppImage
 
 mkdir -p "$BUILD_DIR"
 rm -rf "$BUILD_DIR"/*
--- version.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-cd "$(dirname $0)/source"
-v=$(git describe --long --match "release-*")
-echo ${v#release-}
