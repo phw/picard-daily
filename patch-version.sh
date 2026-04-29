2f1bca64773fb6205740d4871ceecdc2244a4efd
Philipp Wolfer <ph.wolfer@gmail.com>
2026-04-29 17:52:29 +0200

    Use dot to separate count and commit hash in patch-version.sh

--- patch-version.sh
+++ patch-version.sh
@@ -6,6 +6,6 @@ RELEASE_TAG=$(git describe --match "release-*" --abbrev=0 --always HEAD)
 CHANGE_COUNT=$(git rev-list --count "$RELEASE_TAG..HEAD")
 COMMIT_HASH=$(git rev-parse --short HEAD)
 
-python setup.py patch_version --platform="$CHANGE_COUNT-$COMMIT_HASH"
+python setup.py patch_version --platform="$CHANGE_COUNT.$COMMIT_HASH"
 
 echo $(python -c "import picard; print(picard.__version__)")
