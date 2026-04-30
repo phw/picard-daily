1cef008d81c875e4a10242be5451c834329b83bd
Philipp Wolfer <ph.wolfer@gmail.com>
2026-04-30 07:08:29 +0200

    Set patched version only if not directly on a tag

--- patch-version.sh
+++ patch-version.sh
@@ -6,6 +6,11 @@ RELEASE_TAG=$(git describe --match "release-*" --abbrev=0 --always HEAD)
 CHANGE_COUNT=$(git rev-list --count "$RELEASE_TAG..HEAD")
 COMMIT_HASH=$(git rev-parse --short HEAD)
 
-python setup.py patch_version --platform="$CHANGE_COUNT.$COMMIT_HASH"
+if [ "$CHANGE_COUNT" -ne 0 ]; then
+    VERSION_EXTRA="$CHANGE_COUNT.$COMMIT_HASH"
+    echo "Patching version: $VERSION_EXTRA"
+    python setup.py patch_version --platform="$VERSION_EXTRA"
+fi
 
-echo $(python -c "import picard; print(picard.__version__)")
+VERSION=$(python -c "import picard; print(picard.__version__)")
+echo "Patched version: v$VERSION"
